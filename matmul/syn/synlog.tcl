e64857965ff58ed37e5e684dcea94f836f1d6941
hare-care <harisbajramovic2024@u.northwestern.edu>
2023-01-18 12:53:36 -0600

    added synthesis

---
+++ matmul/syn/synlog.tcl
@@ -0,0 +1,10 @@
+history clear
+project -load /home/hbb0163/ce495/CE495/matmul/syn/matmul.prj
+project -run  
+project -run  
+set_option -frequency 1.000000
+set_option -frequency 100
+project -run  
+set_option -frequency auto
+project -run  
+project -close /home/hbb0163/ce495/CE495/matmul/syn/matmul.prj
